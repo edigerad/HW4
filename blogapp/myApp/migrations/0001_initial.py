efe21c2a16a93a7243564cdd393dd7ae4907bfcf
Edige <edige.radolda@ce.sdu.edu.kz>
2015-03-05 14:48:58 +0600

    I created models(post & comment)

---
+++ blogapp/myApp/migrations/0001_initial.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('author', models.CharField(max_length=50)),
+                ('text', models.TextField()),
+                ('pub_date', models.DateTimeField()),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('author', models.CharField(max_length=50)),
+                ('title', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('pub_date', models.DateTimeField()),
+                ('upd_date', models.DateTimeField()),
+                ('is_public', models.BooleanField(default=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(to='myApp.Post'),
+            preserve_default=True,
+        ),
+    ]
